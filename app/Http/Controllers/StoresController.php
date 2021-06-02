4d2f2b56fa72e23952c0888b186b0eb4756283bd
Derkk117 <derkk117@gmail.com>
2021-06-02 09:04:34 -0500

    Create new store with logo and thum

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Store;
 use Auth;
+use App\User;
+use App\Store;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreStore;
 use App\Http\Requests\StoreUpdate;
@@ -24,8 +25,20 @@ class StoresController extends Controller{
     public function store(StoreStore $request){
         $create = function() use ($request){
             try{
+                if($image = $request->file('logo')){
+                    $image_name = "MTL_".date("Y_m_d_H_i_s").".".$image->extension();        
+                    $image->move("StoreImages",$image_name);
+                    $request['image'] = "StoreImages/". $image_name;
+                }
+                if($image = $request->file('thum')){
+                    $image_name = "MTL_thum".date("Y_m_d_H_i_s").".".$image->extension();        
+                    $image->move("StoreImages",$image_name);
+                    $request['thumbnails'] = "StoreImages/". $image_name;
+                }
                 $store = Store::create($request->all());
-                return 'Tienda creada';
+                $user = User::where('id',$request->user_id)->first();
+                $user->update(['store_id'=>$store->id]);
+                return $user;
             }catch(\Exception $e){
                 dd($e);
                 $this->status=500;
